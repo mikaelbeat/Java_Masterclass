92094d62c0d4bd0981c87baa6408077c1aef9fe4
mikaelbeat <ryynanenphm@gmail.com>
2017-09-28 15:35:03 +0300

    Update

---
+++ Java_Masterclass/src/For_Loop.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class For_Loop {
+	
+	static Scanner moneyAmount = new Scanner (System.in);
+	static Scanner intetersRate = new Scanner (System.in);
+	static Scanner startInterest = new Scanner (System.in);
+	static Scanner endInterest = new Scanner (System.in);
+
+	public static void main(String[] args) {
+		
+//		for(int i=2; i<9; i++){
+//			System.out.println("10,000 at "+ i + " interest = " + String.format("%.2f",calculateInterest(10000,i)));
+//		}
+		
+		System.out.println("");
+		
+		for(int i=8; i>=2; i--){
+			System.out.println("10,000 at "+ i + " interest = " + String.format("%.2f",calculateInterest(10000,i)));
+		}
+		
+		System.out.println("");
+		
+		int count = 0;
+		for(int i=10; i<50; i++){
+			if(isPrime(i)){
+				count++;
+				System.out.println("Number "+i+" Is prime number.");
+				if(count == 3){
+					System.out.println("Exiting loop");
+					break;
+				}
+			}
+		}
+	}
+	
+	public static boolean isPrime(int n){
+		if(n == 1){
+			return false;
+		}
+		for(int i=2; i <= n/2; i++){
+			if(n % i == 0){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static double calculateInterest(double amount, double interestRate){
+		return(amount * (interestRate/100));
+	}
+}
