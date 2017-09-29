3ae8305a5679a60b012636035d9de5fbc85aeadb
mikaelbeat <ryynanenphm@gmail.com>
2017-09-29 15:38:34 +0300

    New package

---
+++ Java_Masterclass/src/classes/Car.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class Car {
+
+		private int doors;
+		private int wheels;
+		private String model;
+		private String colour;
+		
+
+
+
+}
---
+++ Java_Masterclass/src/classes/Main.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Car porche = new  Car();
+		Car holden = new Car();
+
+	}
+
+}
