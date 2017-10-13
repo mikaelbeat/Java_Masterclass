805dc272e327f5e81ab55214ab5b1b7b0a74e375
mikaelbeat <ryynanenphm@gmail.com>
2017-10-13 15:56:44 +0300

    Update

---
+++ Java_Masterclass/src/arrayListSolution/Main.java
@@ -0,0 +1,55 @@
+package arrayListSolution;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	private static Scanner scanner = new Scanner(System.in);
+	private static MobilePhone mobilePhone = new MobilePhone("0039 330 4404");
+	
+	public static void main(String[] args) {
+		
+		boolean quit = false;
+		startPhone();
+		printActions();
+		while(!quit){
+			System.out.println("\nEnter action: (6 to show available actions");
+			int action = scanner.nextInt();
+			scanner.nextLine();
+			
+		switch (action){
+		case 0:
+			System.out.println("\nShutting down...");
+			quit = true;
+			break;
+		case 1:
+			printContacts();
+			break;
+		}
+		}
+	}
+	
+	private static void printContacts(){
+		mobilePhone.printContacts();
+	}
+	
+	private static void startPhone(){
+		System.out.println("Starting phone...");
+	}
+	
+	private static void printActions(){
+		System.out.println("\nAvailable actions:\npress");
+		System.out.println("0 - to shutdown\n"+
+						   "1 - to print contacts\n"+
+						   "2 - to add a new contact\n"+
+						   "3 - to update existing contact\n"+
+						   "4 - to remove an existing contact\n"+
+						   "5 - query if an existing contact exists\n"+
+						   "6 - to print a list of available actions");
+		System.out.print("Choose your action: ");
+				
+				
+	
+	}
+
+}
