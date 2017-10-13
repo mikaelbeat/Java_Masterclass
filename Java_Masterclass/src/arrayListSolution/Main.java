d28de81b20f358d1024e0c2e2c498bc36d35c780
mikaelbeat <ryynanenphm@gmail.com>
2017-10-13 16:39:15 +0300

    Update

--- Java_Masterclass/src/arrayListSolution/Main.java
+++ Java_Masterclass/src/arrayListSolution/Main.java
@@ -23,14 +23,38 @@ public class Main {
 			quit = true;
 			break;
 		case 1:
-			printContacts();
+			mobilePhone.printContacts();
+			break;
+		case 2:
+			addNewContact();
+			break;
+		case 3:
+			updateContact();
+			break;
+		case 4:
+			removeContact();
+			break;
+		case 5:
+			queryContact();
+			break;
+		case 6:
+			printActions();
 			break;
 		}
 		}
 	}
 	
-	private static void printContacts(){
-		mobilePhone.printContacts();
+	private static void addNewContact(){
+		System.out.println("Enter new contact name: ");
+		String name = scanner.nextLine();
+		System.out.println("Enter phone number: ");
+		String phone = scanner.nextLine();
+		Contact newContact = Contact.createContact(name, phone);
+		if(mobilePhone.addNewContact(newContact)){
+			System.out.println("New contact added: name = "+name+", phone = "+phone);
+		}else {
+			System.out.println("Cannot add, "+name+" already on file.");
+		}
 	}
 	
 	private static void startPhone(){
