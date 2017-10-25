73e3d0c533ebff116ca0f598b206fb03428b3042
mikaelbeat <ryynanenphm@gmail.com>
2017-10-25 15:50:20 +0300

    Update

---
+++ Java_Masterclass/src/InnerClasses/Main.java
@@ -0,0 +1,13 @@
+package InnerClasses;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Gearbox mcLaren = new Gearbox(6);
+		Gearbox.Gear first = mcLaren.new Gear(1, 12.3);
+		System.out.println(first.driveSpeed(1000));
+
+	}
+
+}
