343d3630c0f4dbfa578302669437e5813b24820c
mikaelbeat <ryynanenphm@gmail.com>
2017-10-27 19:21:59 +0300

    Update

---
+++ Java_Masterclass/src/generics/Main.java
@@ -0,0 +1,27 @@
+package generics;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		ArrayList<Integer> items = new ArrayList<Integer>();
+		items.add(1);
+		items.add(2);
+		items.add(3);
+//		items.add("Snake");
+		items.add(4);
+		items.add(5);
+		
+		printDoubled(items);
+	}
+	
+	private static void printDoubled(ArrayList<Integer> n){
+		for(Object i : n){
+			System.out.println((Integer) i * 2);
+		}
+	}
+}
+
+// 9:18
