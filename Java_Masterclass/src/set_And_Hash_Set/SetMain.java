69f6c41ba2d9e7273c1635cd269a27ed8e82e238
mikaelbeat <ryynanenphm@gmail.com>
2017-11-10 22:06:30 +0200

    Update

--- Java_Masterclass/src/set_And_Hash_Set/SetMain.java
+++ Java_Masterclass/src/set_And_Hash_Set/SetMain.java
@@ -35,6 +35,7 @@ public class SetMain {
 		for(String s : words){
 			System.out.println(s);
 		}
+		System.out.println("===============================================");
 		
 		Set<String> nature = new HashSet<>();
 		Set<String> divine = new HashSet<>();
@@ -43,5 +44,44 @@ public class SetMain {
 		
 		String[] divineWords = {"to", "err", "is", "human", "to", "forgive", "divine"};
 		divine.addAll(Arrays.asList(divineWords));
+		
+		System.out.println("nature - divine:");
+		Set<String>diff1 = new HashSet<>(nature);
+		diff1.removeAll(divine);
+		printSet(diff1);
+		
+		System.out.println("divine - nature:");
+		Set<String>diff2 = new HashSet<>(divine);
+		printSet(diff2);
+		
+		Set<String>unionTest = new HashSet<>(nature);
+		unionTest.addAll(divine);
+		Set<String> intersectionTest = new HashSet<>(nature);
+		intersectionTest.retainAll(divine);
+		
+		System.out.println("Symmetric difference");
+		unionTest.removeAll(intersectionTest);
+		printSet(unionTest);
+		
+		if(nature.containsAll(divine)){
+			System.out.println("divine is a subset of nature");
 		}
-}
+		if(nature.containsAll(intersectionTest)){
+			System.out.println("intersection is subset of nature");
+		}
+		if(divine.containsAll(intersectionTest)){
+			System.out.println("intersection is a subset of divine");
+		}
+	}
+		
+		private static void printSet(Set<String>set){
+			System.out.print("\t");
+			for(String s : set){
+				System.out.print(s+" ");
+			}
+			System.out.println();
+			System.out.println("===============================================");
+		}
+		
+		
+		}
