8f282fb22c455c6f743808afef5f381682a5b47b
Petri Ryynänen <31620690+mikaelbeat@users.noreply.github.com>
2018-01-05 23:11:55 +0200

    Update

--- Java_Masterclass/src/scannerDemo/ScannerFunctions.java
+++ Java_Masterclass/src/scannerDemo/ScannerFunctions.java
@@ -6,44 +6,57 @@ public class ScannerFunctions {
 	
 	 static Scanner reader = new Scanner(System.in);
 	
+	public static void myApplication() {
+		boolean quit = false;
+		int choice = 0;
+		viewMenu();
+		while(!quit){
+			System.out.println("");
+			System.out.print("Choose option: ");
+			choice = reader.nextInt();
+			reader.nextLine();
+			System.out.println("");
+			
+			switch(choice){
+			case 0:
+				viewMenu();
+				break;
+			case 1:
+				askToTypeSomething();
+				break;
+			case 2:
+				addStringToTable();
+				break;
+			case 3:
+				//removeContact();
+				break;
+			case 4:
+				//mobilePhone.viewContacts();
+				break;
+			case 5:
+				System.out.println("Quitting application.");
+				quit = true;
+				break;
+			}
+		}
+	}
+	
+	public static void viewMenu(){
+		System.out.println("0 - to view options menu.\n"+
+				 		 "1 - to try type something.\n"+
+				 		 "2 - to add text to table.\n"+
+				 		 "5 - to quit application"
+				);
+	}
+	
 	public static void askToTypeSomething(){
 		System.out.print("Type something: ");
 		String something = reader.nextLine();
 		System.out.println("You typed: " + something);
 	}
 	
-	public static void menuActions() {
-		boolean quit = false;
-		int choice = 0;
-		printInstructions();
-		while(!quit){
-			System.out.println("");
-			System.out.print("Choose option: ");
-			choice = scanner.nextInt();
-			scanner.nextLine();
-			
-			switch(choice){
-			case 0:
-				printInstructions();
-				break;
-			case 1:
-				addContact();
-				break;
-			case 2:
-				updateContact();
-				break;
-			case 3:
-				removeContact();
-				break;
-			case 4:
-				mobilePhone.viewContacts();
-				break;
-			case 5:
-				turnPhoneOff();
-				quit = true;
-				break;
-			}
-		}
+	public static void addStringToTable() {
+		
 	}
 
 }
--- Java_Masterclass/src/scannerDemo/ScannerSmall.java
+++ Java_Masterclass/src/scannerDemo/ScannerSmall.java
@@ -6,7 +6,7 @@ public class ScannerSmall {
 	
 	public static void main(String[] args) {
 		
-		ScannerFunctions.askToTypeSomething();
+		ScannerFunctions.myApplication();
 
 		
 
