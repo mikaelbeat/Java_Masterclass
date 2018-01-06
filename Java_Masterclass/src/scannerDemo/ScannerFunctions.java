5035363e61b73dff3ed561adf08b2c7eeeb83380
Petri Ryynänen <31620690+mikaelbeat@users.noreply.github.com>
2018-01-06 15:39:49 +0200

    Newest update

--- Java_Masterclass/src/scannerDemo/ScannerFunctions.java
+++ Java_Masterclass/src/scannerDemo/ScannerFunctions.java
@@ -1,12 +1,17 @@
 package scannerDemo;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ScannerFunctions {
 	
-	 static Scanner reader = new Scanner(System.in);
+	static Scanner reader = new Scanner(System.in);
+	
+	private static ArrayList<String> textArray = new ArrayList<String>();
 	
 	public static void myApplication() {
+		textArray.add("Monkey");
+		textArray.add("Snake");
 		boolean quit = false;
 		int choice = 0;
 		viewMenu();
@@ -25,13 +30,13 @@ public class ScannerFunctions {
 				askToTypeSomething();
 				break;
 			case 2:
-				addStringToTable();
+				addStringToArray();
 				break;
 			case 3:
-				//removeContact();
+				showTextArrayContent();
 				break;
 			case 4:
-				//mobilePhone.viewContacts();
+				removeStringFromArray();
 				break;
 			case 5:
 				System.out.println("Quitting application.");
@@ -44,7 +49,9 @@ public class ScannerFunctions {
 	public static void viewMenu(){
 		System.out.println("0 - to view options menu.\n"+
 				 		 "1 - to try type something.\n"+
-				 		 "2 - to add text to table.\n"+
+				 		 "2 - to add string to table.\n"+
+				 		 "3 - show text array content.\n"+
+				 		 "4 - remove string from text array.\n"+
 				 		 "5 - to quit application"
 				);
 	}
@@ -55,8 +62,29 @@ public class ScannerFunctions {
 		System.out.println("You typed: " + something);
 	}
 	
-	public static void addStringToTable() {
+	public static void addStringToArray() {
+		System.out.print("Add text to array: ");
+		String text = reader.nextLine();
+		textArray.add(text);
+		System.out.println(text+" added to array.");
 		
+	}
+	
+	public static void showTextArrayContent() {
+		for(int i=0; i<textArray.size(); i++) {
+			System.out.println("In position "+(1+i)+" is "+textArray.get(i));
+		}
+	}
+	
+	public static void removeStringFromArray() {
+		System.out.println("Strings in text array are: ");
+		for(String i : textArray) {
+			System.out.print(i);
+		}
+		System.out.println("Enter text to be removed: ");
+		String text = reader.nextLine();
+		textArray.remove(text);
+		System.out.println(text+" removed from text array.");
 	}
 
 }
